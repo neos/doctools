3b01abbb4cdc3bf1ba58c539168e4ccc504b6926
Sebastian Kurfuerst <sebastian@typo3.org>
2011-07-29 18:15:21 +0200

    [TASK] basic theming
    Change-Id: I7e95e5e5f809ed13e8ac668cf6c5cd4fd592f437

--- conf.py
+++ conf.py
@@ -103,7 +103,7 @@ lexers['php'] = PhpLexer(startinline=True)
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'agogo'
+html_theme = 'typo3Theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -111,18 +111,18 @@ html_theme = 'agogo'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = [ '.' ]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = project + " " + release
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = 'typo3Theme/static/t3_logo_b_w_inverted.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
